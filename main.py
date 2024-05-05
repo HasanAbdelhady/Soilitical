a08b5d85b2fbea4db84d37ca1e04ff154ce6ed7e
Hasan Abdelhady <84288512+HasanAbdelhady@users.noreply.github.com>
2024-05-05 13:47:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -147,9 +147,8 @@ def quicktry():
     return render_template("quicktry.html")
 
 
-# Run the application
 if __name__ == '__main__':
-    with app.app_context():
+        with app.app_context():
         db.create_all()
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
